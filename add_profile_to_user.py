87cdbc1b0aabbb9bfff658bb878b6c0baacff63d
jon <m@ms-MacBook-Air.local>
2017-03-30 20:49:43 -0400

    fix add profile script

--- add_profile_to_user.py
+++ add_profile_to_user.py
@@ -2,7 +2,6 @@ from forum.models import Profile
 from django.contrib.auth.models import User
 users = User.objects.all().select_related('profile')
 for a_user in users:
-    if not a_user.profile:
-        a_user.profile = Profile.objects.create(user=a_user)
+    a_user.profile = Profile.objects.create(user=a_user)
 
 
