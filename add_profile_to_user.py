9ab250d8ff9cbe7af1052d63449698ed46619f31
George Wang <gwang76@gatech.edu>
2017-04-14 13:58:18 -0400

    moved some files around

--- add_profile_to_user.py
+++
@@ -1,7 +0,0 @@
-from forum.models import Profile
-from django.contrib.auth.models import User
-users = User.objects.all().select_related('profile')
-for a_user in users:
-    a_user.profile = Profile.objects.create(user=a_user)
-
-
